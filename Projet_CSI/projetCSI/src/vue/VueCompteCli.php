a6ba081846778cf3e116a590f257ad36230eb166
Alexandre Sousa <alexandre.sousa8@etu.univ-lorraine.fr>
2021-04-18 17:00:13 +0200

    Afficher ventes Clients

--- Projet_CSI/projetCSI/src/vue/VueCompteCli.php
+++ Projet_CSI/projetCSI/src/vue/VueCompteCli.php
@@ -8,27 +8,28 @@ use projet\vue\VuePrincipaleCli;
 
 class VueCompteCli extends VuePrincipaleCli
 {
-    private $client;
+    private $client, $vente;
 
-    public function __construct($client)
+    public function __construct($client, $vente)
     {
         parent::__construct();
         $this->client = $client;
+        $this->vente = $vente;
     }
 
     public function afficherInformation() {
-        return '<h2 class="col-md-5 control-label"> Information du compte client : '. $this->client[0]['idclient'] . '</h2>'
-            . '<p class="col-md-4 control-label"> Appartient a : '.$this->client[0]['nom'] . ' ' . $this->client[0]['prenom'] . '<br><br>'
-            . 'Email : ' . $this->client[0]['email'] . '<br><br>'
-            . "Date d'inscription" . $this->client[0]['dateinscription'] . '</p>'
-            . '<form class="form-horizontal" action="'. $this->client[0]['idclient'] . '" method="post">
+        return '<h2 class="col-md-5 control-label"> Information du compte client : '. $this->client['idclient'] . '</h2>'
+            . '<p class="col-md-4 control-label"> Appartient a : '.$this->client['nom'] . ' ' . $this->client['prenom'] . '<br><br>'
+            . 'Email : ' . $this->client['email'] . '<br><br>'
+            . "Date d'inscription" . $this->client['dateinscription'] . '</p>'
+            . '<form class="form-horizontal" action="'. $this->client['idclient'] . '" method="post">
                 <fieldset>
                 <!-- Appended Input-->
                 <div class="form-group">
                   <label class="col-md-4 control-label" for="appendedtext">Solde</label>
                   <div class="col-md-4">
                     <div class="input-group">
-                      <input id="nouveausolde" name="nouveausolde" class="form-control" placeholder="' . $this->client[0]['solde'] . '" type="text" required="">
+                      <input id="nouveausolde" name="nouveausolde" class="form-control" placeholder="' . $this->client['solde'] . '" type="text" required="">
                       <button id="boutonmodifiersolde" name="boutonmodifiersolde" class="btn btn-primary">Modifier</button>
                     </div>
                   </div>
@@ -38,14 +39,36 @@ class VueCompteCli extends VuePrincipaleCli
                 ';
     }
 
+    public function afficherVentes() {
+        $res = '';
+        foreach ($this->vente as $vente) {
+            if($vente['idclient'] == $this->client['idclient']) {
+                $res .= 'Id vente : ' . $vente['idvente'] . ' - Montant : ' . $vente['montant'] . ' - Bénéfice : ' . $vente['benefice'] . ' - IdLot : ' . $vente['idlot'] . ' - IdClient du gagnant : ' . $vente['idclient']. '<br>';
+            }
+        }
+        return $res;
+    }
+
     public function render()
     {
         $menu = self::getMenu();
         $footer = self::getFooter();
-        $content = self::afficherInformation();
+        $informationCompte = self::afficherInformation();
+        $ventes = self::afficherVentes();
         $html = <<<END
             $menu
-            $content
+            $informationCompte
+            <h3 class="col-md-5 control-label">Les différentes ventes</h3>
+            <div class="col-md-5 control-label">
+                <div class=\"container h - 100\">
+                    <div class=\"row h - 100 align - items - center\">
+                       <div class=\"col - 12 text - center\">
+                            $ventes
+                       </div>
+                    </div>
+                </div>
+            </div>
+            
             $footer
         END;
         echo $html;
